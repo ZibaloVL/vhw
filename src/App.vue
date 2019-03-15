f484ce88b38cb76a1c80932665b8a9ef9e0d618d
ZibaloV <fotoroom.md@gmail.com>
2019-03-15 12:49:20 +0200

    переделываю форму

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+   <div v-for="(punckt, index) in punckts " :key="index">
+     <h2>{{ punckt.title }}</h2>
+    
+      <div  class = "form-check" style = "text-align: left"
+        v-for="(answer, index_answer) in punckt.answers"  :key="index_answer">      
+        <input  class = "form-check-input"
+            :type="punckt.type" 
+           :value="answer"
+           v-model="choise_answer[index]"
+           >
+         <label class="form-check-label">{{answer}}</label>  
+      </div>
+
+      <div class="form-check">
+        <label  class="form-check-input"
+         v-for="(answer, index_answer) in punckt.answers"  :key="index_answer">  
+         <input  class = "form-check-input"
+            :type="punckt.type" 
+            :value="answer"
+            v-model="choise_answer[index]"
+           >
+          {{answer}}
+        </label>
+      </div>
+
+
+    
+       
+     
+
+   </div>
   </div>
 </template>
 
@@ -24,7 +39,30 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      choise_answer:[],
+      punckts: [
+                {
+                  type: 'radio',
+                  title: 'Какой тег задаёт ссылку?',
+                  answers: [
+                    'a',
+                    'div',
+                    'span',
+                    'img'
+                  ]
+                },
+
+                {
+                  type: 'checkbox',
+                  title: 'Какие из  этих тегов строчные?',
+                  answers: [
+                    'a',
+                    'div',
+                    'span',
+                    'img'
+                  ]
+                }
+              ]
     }
   }
 }
