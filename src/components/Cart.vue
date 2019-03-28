e4614167f2f004decea33342d488ae3c225d8e01
ZibaloV <fotoroom.md@gmail.com>
2019-03-28 07:02:11 +0200

    добавил кнопку

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,15 +1,19 @@
 <template>
     <div>  
-    <h2>{{punckt.title}}</h2>
-    
-    <div class="form-check">
-      <div v-for="(answer, index) in punckt.answers" :key="index">  
-        <label class="form-check-label">
-            <input :type="punckt.type" class="form-check-input"   :value="answer">
-            {{answer}}
-        </label>
-      </div>
-    </div>
+        <form action="">
+            <h2>{{punckt.title}}</h2>
+            <hr>
+            <div class="form-check">
+                <div v-for="(answer, index) in punckt.answers" :key="index">  
+                    <label class="form-check-label">
+                        <input :type="punckt.type" class="form-check-input"   :value="answer">
+                        {{answer}}
+                    </label>
+                </div>
+            </div>
+            <hr>
+            <button type="submit" class="btn btn-primary btn-sm">send</button>
+        </form>
     </div>
 </template>
 
